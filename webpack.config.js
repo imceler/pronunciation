e95ba7841dbcac4dfcc5071c005b35365e04ba40
Gustavo Cañas <ghenriquez700@gmail.com>
2019-10-09 18:55:42 -0400

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserJSPlugin = require('terser-webpack-plugin') 
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname,'src/js/index.js'),
-    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name].js',
-        publicPath: 'http://localhost:1234/',
+        filename: 'js/[name].[hash].js',
+        // publicPath: 'http://localhost:1234/',
+        // publicPath: 'pronunciation',
         chunkFilename: 'js/[id].[chunkhash].js'
     },
-    devServer: {
-        open: true,
-        port: 1234
-    },
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+      },
     module: {
       rules: [
             {
@@ -23,21 +27,23 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-	            use: [
-                    'style-loader',
-                    {
-		            loader: 'css-loader',
-		            options: {
-		                importLoaders: 1
-		             }
-	                },      
-                    'postcss-loader'
+                use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                },
+                    // 'style-loader',
+                    'css-loader'
                 ]
             },
             {
                 test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
                 use: {
-                    loader: 'file-loader', 
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1000,
+                        name: '[hash].[ext]',
+	                    outputPath: 'assets'
+                    }
                 }
             }
       ]  
@@ -45,7 +51,18 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin ({
             title: 'Pronunciation',
-            template: path.resolve(__dirname, 'dist/index.html')
-        })
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[hash].css',
+            chunkFilename: 'css/[id].[hash].css'
+          }),
+          new AddAssetHtmlPlugin({
+            filepath: path.resolve(__dirname, 'dist/js/*.js')
+        }),
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: ["**/main.*"]
+          })
     ]
 }
---
+++ webpack.dev.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: path.resolve(__dirname,'src/js/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js',
+        publicPath: 'http://localhost:1234/',
+        chunkFilename: 'js/[id].[chunkhash].js'
+    },
+    devServer: {
+        open: true,
+        port: 1234
+    },
+    module: {
+      rules: [
+            {
+                test: /\.js$/, 
+                use: 'babel-loader', 
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+	            use: [
+                    'style-loader',
+                    {
+		            loader: 'css-loader',
+		            options: {
+		                importLoaders: 1
+		             }
+	                },      
+                    'postcss-loader'
+                ]
+            },
+            {
+                test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
+                use: {
+                    loader: 'file-loader', 
+                }
+            }
+      ]  
+    },
+    plugins: [
+        new HtmlWebpackPlugin ({
+            title: 'Pronunciation',
+            template: path.resolve(__dirname, 'dist/index.html')
+        })
+    ]
+}
